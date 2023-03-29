645269534deeb8a21cf4fbdfbd91a93ceb26a73a
ivanaplese <ivanaplese123@gmail.com>
2023-03-29 18:50:17 +0200

    PRVICOMMIT

--- kontrolna.js
+++ kontrolna.js
@@ -1,1 +1,10 @@
-// kontrolna
+// kontrolna GRUPA A
+
+
+//1. Napisi funkciju koja ispisuje parne brojeve od 0 do 100 (ukljucujuci 100)
+
+function ispis_brojeva() {
+    for (let i = 0; i <= 100; i++) {
+        console.log(i);
+    }
+}
